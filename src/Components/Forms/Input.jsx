762e7592280cc35b0dca62ab4d3dc2686e421d6f
Alan Barbosa <alanzinhob@hotmail.com>
2023-08-05 11:12:03 -0300

    05_8

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,10 +1,12 @@
 import styles from './Input.module.css'
 
-export function Input({type,text,name,placeholder,handleOnChange,value}) {
+export function Input({type,text,name,placeholder,handleOnChange,value,icon}) {
     return(
         <div className={styles.form_control}>
-            <label htmlFor={name}>{text}:</label>
-            <input 
+            <label htmlFor={name}>
+						<i class={icon}></i>
+						{text}:</label>
+            <input'1
             type={type}
             name={name}
             id={name}
